c4702825e0249485046ed6ebe186cdc67cc8b660
RJ2607 <rishijain2607@gmail.com>
2023-09-26 23:35:49 +0530

    1

---
+++ sonar.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+#loading the dataset to a pandas DataFrame
+sonar_data = pd.read_csv('sonar rock vs mine\sonar data.csv', header=None)
+#print(sonar_data.head())
