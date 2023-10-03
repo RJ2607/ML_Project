4e80e8fd7e03d65913ccf5427658cf049c75e001
RJ2607 <rishijain2607@gmail.com>
2023-10-03 22:47:28 +0530

    bugs

--- Diabetes prediction/diabetes.py
+++ Diabetes prediction/diabetes.py
@@ -37,17 +37,17 @@ accuracy_test = accuracy_score(X_test_accuracy, Y_test)
 
 #predictive system
 #input data
-input_data = (7,187,68,39,304,37.7,0.254,41)
+input_data = (2,84,0,0,0,0,0.304,21)
 input_data_as_numpy_array = np.asarray(input_data)
  #reshape numpy array
 input_data_reshape = input_data_as_numpy_array.reshape(1,-1)
 
 #standardize the input data
-scaler.fit(input_data_reshape)
-input_data_reshape = scaler.transform(input_data_reshape)
+
+input_data_reshape1 = scaler.transform(input_data_reshape)
 
 #prediction
-prediction = classifier.predict(input_data_reshape)
+prediction = classifier.predict(input_data_reshape1)
 
 if (prediction[0] == 0):
     print("The person is not diabetic")
